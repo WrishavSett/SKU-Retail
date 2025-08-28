946819b3bebb08353ecbcdd3b5d7c6f10d83d3cf
WrishavSett <wrishavsett2912@gmail.com>
2025-08-28 15:43:18 +0530

    Updates

---
+++ src/semantic.py
@@ -0,0 +1,11 @@
+t_itemdesc = "SOFTY BABY PANT/M CRAWLER/4 PICES/PPH"
+m_sku_list = {1: "SOFTY BABY PANT-SMALL(S) PPH 4 NOS", 
+              2: "SOFTY BABY PANT-MEDIUM(M) PPH 4 NOS", 
+              3: "SOFTY BABY PANT-LARGE(L) PPH 4 NOS", 
+              4: "TENDER TOUCH BABY PULL UP PANTS-MEDIUM(M) PPH 5 NOS", 
+              5: "TENDER TOUCH BABY PULL UP PANTS-SMALL(S) PPH 5 NOS"}
+
+import re
+import json
+import ollama
+
